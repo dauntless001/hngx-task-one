bde5ee1623fbc11bab6e8b4109841ca772237e5b
theWiz01 <omatanmi118@gmail.com>
2023-09-12 22:57:09 +0100

    jhjd

--- hngx/views.py
+++ hngx/views.py
@@ -1,12 +1,19 @@
 from django.http import JsonResponse
+
+from datetime import timezone
 import datetime
+
+dt = datetime.datetime.now(timezone.utc)
+  
+utc_time = dt.replace(tzinfo=timezone.utc)
+utc_timestamp = utc_time.timestamp()
 
 def home(request):
     return JsonResponse({
         "slack_name" : "Wizz",
         "track" : "backend",
         "current_day" : "Tuesday",
-        "utc_time" : "2023-09-12T22:10:11Z",
+        "utc_time" : utc_timestamp,
         "status_code" : 200,
         "github_file_url" : "https://github.com/dauntless001/hngx-task-one/blob/master/hngx/views.py",
         "github_repo_url" : "https://github.com/dauntless001/hngx-task-one"
