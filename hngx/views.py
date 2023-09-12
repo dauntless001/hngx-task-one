13be9ace151cb09d0058369862a031bde078f15c
theWiz01 <omatanmi118@gmail.com>
2023-09-12 22:47:25 +0100

    ahs

--- hngx/views.py
+++ hngx/views.py
@@ -4,9 +4,9 @@ import datetime
 def home(request):
     return JsonResponse({
         "slack_name" : "Wizz",
-        "track" : "Backend",
+        "track" : "backend",
         "current_day" : "Tuesday",
-        "utc_time" : datetime.datetime.now(),
+        "utc_time" : "2023-09-12T22:03.28Z",
         "status_code" : 200,
         "github_file_url" : "https://github.com/dauntless001/hngx-task-one/blob/master/hngx/views.py",
         "github_repo_url" : "https://github.com/dauntless001/hngx-task-one"
